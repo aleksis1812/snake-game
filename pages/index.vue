732554057fd39bc745e1d003e594279d29823a8c
Aleksandra Markovic <aleksandra.markovic.1812@gmail.com>
2024-02-06 21:34:56 +0100

    Initial grid

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    style="display: flex; flex-direction: column; margin: 0; min-height: 98vh"
+  >
+    <div
+      v-for="row in rows"
+      style="
+        display: table;
+        border-collapse: collapse;
+        table-layout: fixed;
+        width: 100%;
+        flex: 1;
+      "
+    >
+      <div
+        v-for="col in columns"
+        style="display: table-cell; border: 1px solid red"
+      >
+        <!-- <span style="color: white; display: block">pp</span> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const rows = ref(20);
+const columns = ref(50);
+</script>
